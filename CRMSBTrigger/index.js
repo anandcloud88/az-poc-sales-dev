cd16e9f9ced17ba721a86a4eee762ccef63a1f61
anicloud789 <kumar.anand.cloud2@gmail.com>
2023-10-09 18:54:05 +0530

    test

--- CRMSBTrigger/index.js
+++ CRMSBTrigger/index.js
@@ -1,3 +1,3 @@
 module.exports = async function(context, mySbMsg) {
-    context.log('JavaScript ServiceBus queue trigger function processed message', mySbMsg);
+    context.log('JavaScript ServiceBus queue trigger function processed message hello', mySbMsg);
 };
